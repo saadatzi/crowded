3f494271ce1a6ec986e73f8e7332533192a6336b
Payam Chamani <pchamani@nizek.com>
2020-04-02 13:57:42 +0430

    logs relative path for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,9 +20,9 @@ module.exports = {
             env_production: {
                 NODE_ENV: 'production'
 			},
-			error_file: 'logs/err.log',
-			out_file: 'logs/out.log',
-			log_file: 'logs/log.log',
+			error_file: './logs/err.log',
+			out_file: './logs/out.log',
+			log_file: './logs/log.log',
 			merge_logs: true
         },
         // {
