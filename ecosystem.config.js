0683da1c89d9ace3e02bcbe8d7c941ed3f21398e
Payam Chamani <pchamani@nizek.com>
2020-04-02 12:25:37 +0430

    logs for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,7 +19,11 @@ module.exports = {
             },
             env_production: {
                 NODE_ENV: 'production'
-            }
+			},
+			error_file: 'logs/err.log',
+			out_file: 'logs/out.log',
+			log_file: 'logs/log.log',
+			merge_logs: true
         },
         {
             cwd: './Report_CMS',
