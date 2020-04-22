9db4822b6cca2319de30877a11fbc66fb5944376
Mehdi Kazem Nia <mehdi@Nizek-iMac-1.local>
2020-04-22 21:32:57 +0430

    getById and getMany for BankName

--- core/models/BankName.js
+++ core/models/BankName.js
@@ -3,11 +3,11 @@ const Schema = mongoose.Schema;
 const BankNameSchema = new Schema({
     name_en: { type: String, required: true },
     name_ar: { type: String, required: true },
-    status: {type: Number, default: 1}, // 1 active, 0 deActive, 2 softDelete, 3 hardDelete
+    status: { type: Number, default: 1 }, // 1 active, 0 deActive, 2 softDelete, 3 hardDelete
 },
-{
-    timestamps: true
-});
+    {
+        timestamps: true
+    });
 
 
 
@@ -21,6 +21,48 @@ BankNameSchema.method({
  * Statics
  */
 BankNameSchema.static({
+    /**
+    * Find BankName
+    *
+    * @param {ObjectId} _id
+    * @api private
+    */
+    getById(_id) {
+
+        return this.findById({ _id }).exec()
+    },
+    /**
+     * Find BankNames (plural)
+     *
+     * @param {Object} options
+     * @api private
+     */
+    getMany(options) {
+
+        let lang = options.lang;
+        let modifiedCriteria = {};
+
+        return this.aggregate([
+            {
+                $match: modifiedCriteria
+            },
+            {
+                $project: {
+                    _id: 0,
+                    id: "$_id",
+                    name: `$name_${lang}`
+                }
+            }
+        ])
+            .exec()
+            .catch(err => {
+                console.error(`!!!BankName getMany failed with options: ${JSON.stringify(options)}`, err);
+            });
+
+
+    },
+
+
 });
 
 const BankName = mongoose.model('BankName', BankNameSchema);
