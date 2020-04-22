61d7b515603f0d3e80a5eecb2dde0a09720226ca
Mehdi Kazem Nia <mehdi@Nizek-iMac-1.local>
2020-04-22 21:34:41 +0430

    bankAccountController C&R implemented

--- core/controllers/bankAccount.js
+++ core/controllers/bankAccount.js
@@ -8,140 +8,58 @@ const bankAccountController = function () {
 };
 
 /**
- * Add new Area
+ * Add new BankAccount
  *
  * @param {Object || Array} newBankAccount
  *
  * @return {ObjectId} bankAccountId
  */
-bankAccountController.prototype.add = async (newBankAccount) => {
-    if (Array.isArray(newBankAccount)) { //newArea instanceof Array
-        return await BankAccount.insertMany(newBankAccount)
-            .then(() => {})
+bankAccountController.prototype.add = async (userId, newBankAccount) => {
+    let payload = {
+        userId,
+        ...newBankAccount
+    }
+    return await BankAccount.create(payload)
+        .then(bankaccount => {
+            console.log("***BankAccount save success bankaccount._id", bankaccount._id);
+            return bankaccount;
+        })
+        .catch(err => {
+            console.error("!!!BankAccount save failed: ", err);
+            throw err;
+        })
+};
+
+
+/**
+ * Get bank Accounts
+ *
+ * @return {Array} bankAccountId
+ */
+bankAccountController.prototype.get = async (optFilter) => {
+    // TODO: figure out how to pass lang for single fetch
+    if (!optFilter || optFilter instanceof Object) {
+        return await BankAccount.getAll(optFilter)
+            .then(result => {
+                console.log(`*** Fetched ${result.length} Accounts  by filter %j`, optFilter);
+                return result;
+            })
             .catch(err => {
-                console.error("!!!BankAccount many save failed: ", err);
-                return -1;
+                console.log("!!!BankAccount getAll failed: ", err);
+                throw err;
             })
     } else {
-        return await BankAccount.create(newBankAccount)
-            .then(bankaccount => {
-                console.log("***BankAccount save success bankaccount._id", bankaccount._id);
-                return bankaccount;
+        return await BankAccount.getById(optFilter)
+            .then(result => {
+                console.log(`***BankAccount getById ${optFilter} result: `, result);
+                return result;
             })
             .catch(err => {
-                console.error("!!!BankAccount save failed: ", err);
+                console.log("!!!BankAccount getById failed: ", err);
                 return -1;
             })
     }
 };
 
-// /**
-//  * get Area
-//  *
-//  * @param {Object || ObjectId} optFilter
-//  *
-//  * @return Area
-//  */
-// areaController.prototype.get = async (optFilter) => {
-//     if (!optFilter || optFilter instanceof Object) { //newArea instanceof Array
-//         return await Area.list()
-//             .then(interests => {
-//                 let returnedAreas = [];
-//                 interests.map(interest => returnedAreas.push(interest.transform(optFilter.selected, optFilter.lang)));
-//                 return returnedAreas;
-//             })
-//             .catch(err => {
-//                 console.log("!!!Area getAll failed: ", err);
-//                 throw err;
-//             })
-//     } else {
-//         return await Area.get(optFilter)
-//             .then(result => {
-//                 console.log(`***Area get by id ${optFilter} result: `, result);
-//                 return result;
-//             })
-//             .catch(err => {
-//                 console.log("!!!Area get failed: ", err);
-//                 return -1;
-//             })
-//     }
-// };
-
-// /**
-//  * remove Area
-//  *
-//  * @param {Object || ObjectId} optFilter
-//  *
-//  * @return Query
-//  */
-// areaController.prototype.remove = async (optFilter) => {
-//     if (optFilter) {
-//         if (optFilter instanceof Object) { //instanceof mongoose.Types.ObjectId
-//             return await Area.remove(optFilter)
-//                 .then(result => {
-//                     console.log("***Area  Remove many result: ", result);
-//                     return result;
-//                 })
-//                 .catch(err => {
-//                     console.log("!!!Area Remove failed: ", err);
-//                     return -1;
-//                 })
-//         } else {
-//             return await Area.findByIdAndRemove(optFilter)
-//                 .then(result => {
-//                     console.log(`***Area Remove by id ${optFilter} result: `, result);
-//                     return result;
-//                 })
-//                 .catch(err => {
-//                     console.log("!!!Area Remove failed: ", err);
-//                     return -1;
-//                 })
-//         }
-//     } else {
-//         throw {errMessage: 'for remove Object conditions or Id is required!'}
-//     }
-
-
-// };
-
-// /**
-//  * Update Area
-//  *
-//  * @param {Object || ObjectId} optFilter
-//  * @param {Object} newValue
-//  *
-//  * @return Query
-//  */
-// areaController.prototype.update = async (optFilter, newValue) => {
-//     if (optFilter) {
-//         if (optFilter instanceof Object) { //instanceof mongoose.Types.ObjectId
-//             //ToDo return Query?!
-//             return await Area.updateMany(optFilter, newValue)
-//                 .then(result => {
-//                     console.log("***Area  Update many result: ", result);
-//                     return result;
-//                 })
-//                 .catch(err => {
-//                     console.log("!!!Area Update failed: ", err);
-//                     return -1;
-//                 })
-//         } else {
-//             //ToDo return Query?!
-//             return await Area.findByIdAndUpdate(optFilter, newValue)
-//                 .then(result => {
-//                     console.log(`***Area Update by id ${optFilter} result: `, result);
-//                     return result;
-//                 })
-//                 .catch(err => {
-//                     console.log("!!!Area Update failed: ", err);
-//                     return -1;
-//                 })
-//         }
-//     } else {
-//         throw {errMessage: 'for Update Object conditions or Id is required!'}
-//     }
-
-
-// };
 
 module.exports = new bankAccountController();
