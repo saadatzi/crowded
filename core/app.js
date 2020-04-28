fe5a17789bbb3c949af09687fbb3d99284cf8376
Payam Chamani <pchamani@nizek.com>
2020-04-28 12:20:38 +0430

    cors UPdated for PUT and DELETE

--- core/app.js
+++ core/app.js
@@ -70,7 +70,7 @@ app.use(function (req, res, next) {
     if (allowedOrigins.indexOf(origin) > -1) {
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-PySign, X-PyMod, PyToken, X-PyReq');
     res.header('Access-Control-Allow-Credentials', true);
     if(req.method === 'OPTIONS'){
