81fd4fcc0d654bc7d87be70bc3b59d16d9c6be7a
Mehdi Kazem Nia <mehdi@Nizek-iMac-1.local>
2020-04-30 14:51:50 +0430

    Organization edit

--- core/routing/panel/organization.js
+++ core/routing/panel/organization.js
@@ -1,28 +1,30 @@
 const express = require('express')
     , router = express.Router();
-const {uploader} = require('../../utils/fileManager');
+const { uploader } = require('../../utils/fileManager');
 
 // Instantiate the Device Model
 const organizationController = require('../../controllers/organization');
 const NZ = require('../../utils/nz');
-const {verifyTokenPanel} = require('../../utils/validation');
+const { verifyTokenPanel } = require('../../utils/validation');
 
 const Joi = require('@hapi/joi');
 const JoiConfigs = require('./../joiConfigs');
-const {joiValidate} = require('./../utils');
+const { joiValidate } = require('./../utils');
 
 
 
 const addSchema = Joi.object().keys({
-    title:           JoiConfigs.title,
-    address:         JoiConfigs.title,
-    phones:             Joi.array().items(JoiConfigs.phone),
+    title: JoiConfigs.title,
+    address: JoiConfigs.title,
+    phones: Joi.array().items(JoiConfigs.phone),
 });
 
 
 const updateSchema = Joi.object().keys({
-    organizationId: JoiConfigs.isMongoId,
-    name: JoiConfigs.title,
+    id: JoiConfigs.isMongoId,
+    title:           JoiConfigs.title.optional(),
+    address:         JoiConfigs.title.optional(),
+    phones:             Joi.array().items(JoiConfigs.phone).optional(),
 });
 
 const hasValidIdSchema = Joi.object().keys({
@@ -37,7 +39,7 @@ const hasValidIdSchema = Joi.object().keys({
  */
 //______________________Add Organization_____________________//
 router.post('/add', uploader, joiValidate(addSchema), verifyTokenPanel(), async (req, res) => {
-    console.info('API: Add Organization/init %j', {body: req.body});
+    console.info('API: Add Organization/init %j', { body: req.body });
 
     if (!req._uploadPath || !req._uploadFilename) {
         return new NZ.Response(null, 'fileUpload is Empty!', 400).send(res);
@@ -61,11 +63,18 @@ router.post('/add', uploader, joiValidate(addSchema), verifyTokenPanel(), async 
  * -update Organization in db
  * @return status
  */
-//______________________Update Organization_____________________//
-router.put('/update', joiValidate(updateSchema), verifyTokenPanel(), async (req, res) => {
-    console.info('API: update Organizatio n/init %j', {body: req.body});
+router.put('/edit', uploader, joiValidate(updateSchema), verifyTokenPanel(), async (req, res) => {
+    console.info('API: update Organizationn %j', { body: req.body });
 
-    organizationController.update(req.body.organizationId, req.body.permissions)
+    if (req._uploadPath && req._uploadFilename) {
+        req.body.image = req._uploadPath + '/' + req._uploadFilename;
+    }
+    // anyways 
+
+    const id = req.body.id;
+    delete req.body.id;
+
+    organizationController.update(id, req.body)
         .then(organization => {
             new NZ.Response(null, organization ? 'Organization has been successfully update!' : 'Not found!', organization ? 200 : 404).send(res);
         })
@@ -96,10 +105,10 @@ router.post('/', verifyTokenPanel(), async (req, res) => {
  *  Get Organization
  * @return Organizations
  */
-router.get('/:id', verifyTokenPanel(), joiValidate(hasValidIdSchema,2) ,async (req, res) => {
+router.get('/:id', verifyTokenPanel(), joiValidate(hasValidIdSchema, 2), async (req, res) => {
     console.info('API: Get Organization');
 
-    organizationController.getOnePanel({_id:req.params.id})
+    organizationController.getOnePanel({ _id: req.params.id })
         .then(organization => {
             new NZ.Response(organization).send(res);
         })
