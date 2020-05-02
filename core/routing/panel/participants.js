780e84cae58d4856eedaa292a26685fade414c02
s.m.hejazi <hejazi.ppp@gmail.com>
2020-05-02 11:00:28 +0430

    fix manage participants

--- core/routing/panel/participants.js
+++ core/routing/panel/participants.js
@@ -6,7 +6,7 @@ const JoiConfigs = require('../joiConfigs');
 const {joiValidate} = require('../utils');
 
 // Instantiate the Device Model
-const eventController = require('../../controllers/event');
+const userEventController = require('../../controllers/userEvent');
 const userController = require('../../controllers/user');
 const deviceController = require('../../controllers/device');
 const NZ = require('../../utils/nz');
@@ -21,7 +21,7 @@ const locationSchema = Joi.object().keys({
 
 
 // Joi valdiator schemas
-
+//TODO add validation JOI
 
 /**
  * Get Event Participants
@@ -34,6 +34,25 @@ router.post('/', verifyTokenPanel(), authorization([{EVENT: 'R'}, {USER: 'R'}, {
     userController.getParticipants(req.admin, req.body, req.auth)
         .then(items => {
             new NZ.Response({items}).send(res);
+        })
+        .catch(err => {
+            console.error("Get Participants Catch err:", err);
+            new NZ.Response(null, err.message, err.code || 500).send(res);
+        })
+});
+
+
+/**
+ * Set Event Participants
+ * @return Boolean
+ */
+//______________________Get Participants Event_____________________//
+router.post('/manage', verifyTokenPanel(), authorization([{EVENT: 'R'}, {USER: 'R'},{MANAGE_PARTICIPANTS: 'U'}]), async (req, res) => {
+    console.info('API: Get Participants event/init %j', {body: req.body});
+
+    userEventController.manageParticipant(req.admin, req.body, req.auth)
+        .then(item => {
+            new NZ.Response(true, 'Your request has been successfully submitted').send(res);
         })
         .catch(err => {
             console.error("Get Participants Catch err:", err);
