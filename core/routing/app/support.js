6e6c6bfe3999a41f4dbeb131ca171538b5859972
Mehdi Kazem Nia <mehdi@Nizek-iMac-1.local>
2020-05-04 14:02:30 +0430

    support mail done (ugly template though)

--- core/routing/app/support.js
+++ core/routing/app/support.js
@@ -2,6 +2,10 @@ const express = require('express')
     , router = express.Router();
 
 const Joi = require('@hapi/joi');
+const JoiConfigs = require('../joiConfigs');
+const {joiValidate} = require('../utils');
+
+
 
 // Instantiate the Device Model
 const supportController = require('../../controllers/support');
@@ -10,15 +14,48 @@ const deviceController = require('../../controllers/device');
 const NZ = require('../../utils/nz');
 const {verifyToken} = require('../../utils/validation');
 
+// Models
+const User = require('../../models/User');
+
+
+
+const addSchema = Joi.object().keys({
+    message: JoiConfigs.strOptional,
+    name: JoiConfigs.title,
+});
+
 /**
  *  Add Support
  * -add Support in db
  * @return status
  */
 //______________________Add Support_____________________//
-router.post('/add', verifyToken(true), async (req, res) => {
+router.post('/add', verifyToken(),  async (req, res) => {
     console.info('API: Add support/init %j', {body: req.body});
-    new NZ.Response(true, 'Your message has been successfully submitted!').send(res);
+    let payload = {
+        message: req.body.message,
+        name: req.body.name,
+        email: req.body.email
+    }
+    if(req.userId){
+        let user = await User.getById(req.userId);
+        if(!user) return new NZ.Response(null, "Invalid user!", 400).send(res);
+        // else
+        payload.email = user.email;
+    }
+    // if OK
+    if(!payload.email) return new NZ.Response(null, "Please include your email address!", 400).send(res);
+
+    return supportController.add(payload)
+        .then((ha)=>{
+            new NZ.Response(null, 'Your message has been successfully submitted!').send(res);
+        })
+        .catch(err=>{
+            return console.error(err);
+            new NZ.Response(null, err.message, err.code).send(res);
+        });
+    
+   
 });
 
 /**
