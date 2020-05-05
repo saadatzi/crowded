cd7eeeafec9f360f06937d37c567faa1f48c0922
s.m.hejazi <hejazi.ppp@gmail.com>
2020-05-05 05:04:25 +0430

    add pagination participants.js

--- core/models/Permission.js
+++ core/models/Permission.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const PermissionSchema = new Schema({
     title: {type: String, index: true, uppercase: true, unique: true, required: [true, "can't be blank"]},
-    access: {type: Number, default: 0}
+    access: {type: Number, default: 0},
+    order: Number
 });
 
 
@@ -109,6 +110,7 @@ PermissionSchema.static({
      */
     list: async function () {
         return await this.find({})
+            .sort({order: 1})
             .then(permissions => permissions)
             .catch(err => console.log("permissions getAll Catch", err));
     },
