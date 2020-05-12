67e0d5c8339bb243f92af204d862a3ec01120453
Mehdi Kazem Nia <mehdi@Nizek-iMac-1.local>
2020-05-12 15:51:47 +0430

    cacheLayer bug fixed

--- core/utils/cacheLayer.js
+++ core/utils/cacheLayer.js
@@ -21,16 +21,16 @@ const insertForgotHash = async (hash, userId) => {
 
 
 const getForgotHash = async (hash) => {
-    return new Promise(resolve => {
+    return new Promise((resolve,reject) => {
         hashForgotPassCache.get(hash, async function (err, result) {
             if (err) {
-                throw err
+                reject(err)
             }
             if (result) {
                 // hashForgotPassCache.del(hash, function(err) {console.error("!!!! remove hash forgot Password catch err:", err)});
                 resolve(result);
             }
-            throw {code: 400, message: 'The link has expired or invalid request!'}
+            reject({code: 400, message: 'The link has expired or invalid request!'});
         });
     });
 };
