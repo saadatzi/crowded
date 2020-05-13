af8a5f5dc24bc101cbc77bbd07bec39617ef76f4
s.m.hejazi <hejazi.ppp@gmail.com>
2020-05-13 16:14:24 +0430

    fix imagePicker

--- core/models/Event.js
+++ core/models/Event.js
@@ -770,6 +770,7 @@ EventSchema.static({
                     _id: "$_id",
                     isActive: {$first: "$status"},
                     images: {$push: {id: '$images._id',url: {$concat: [settings.media_domain, "$images.url"]}, order: "$images.order"}}, //$push
+                    imagePicker: {$first: `$imagePicker`},
                     title_en: {$first: `$title_en`},
                     title_ar: {$first: `$title_ar`},
                     desc_en: {$first: `$desc_en`},
