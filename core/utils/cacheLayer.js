14e751ca42a411bf2ce99294e0f217eec7d09e05
Administrator <pchamani@nizek.com>
2020-05-15 22:49:10 +0430

    aa

--- core/utils/cacheLayer.js
+++ core/utils/cacheLayer.js
@@ -19,7 +19,7 @@ const setHash = async (hash, userId) => {
     hashForgotPassCache.set(hash, userId)
 };
 
-hashForgotPassCache.set('pach', '5eba2bc14532730ea20350c6')
+hashForgotPassCache.set('pach', '5ebb3ed24f7f7a13247476ea')
 
 
 const getHash = async (hash, shouldRemove) => {
