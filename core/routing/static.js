e353779b43a41c42bab168c0ae8fa80b3929e485
s.m.hejazi <hejazi.ppp@gmail.com>
2020-05-15 15:17:01 +0430

    fix chart rout

--- core/routing/static.js
+++ core/routing/static.js
@@ -5,23 +5,6 @@ const settings = require('../utils/settings');
 const staticController = require('../controllers/static');
 const transactionController = require('../controllers/transaction');
 
-app.get('/:lang/:alias/', async (req, res, next) => {
-
-	const page = await staticController.getByAlias(req.params.alias);
-	if (!page) return next();
-
-	const html = {
-		title:	 req.params.lang == 'ar' ? page.name_ar : page.name_en,
-		content: req.params.lang == 'ar' ? page.html_ar : page.html_en,
-	};
-
-	res.render('static_page', {
-		project_name:	settings.project_name,
-		title:			html.title,
-		content:		html.content,
-	});
-});
-
 /**
  * Get Wallet Chart
  * @return Chart html
@@ -44,11 +27,23 @@ app.get('/myWalletChart/:hash', async function (req, res) {
 			console.error("Get appMyWalletChart Catch err:", err);
 			new NZ.Response(null, err.message, 500).send(res);
 		})
-	/*res.render('my_wallet_chart', {
+});
+
+app.get('/:lang/:alias/', async (req, res, next) => {
+
+	const page = await staticController.getByAlias(req.params.alias);
+	if (!page) return next();
+
+	const html = {
+		title:	 req.params.lang == 'ar' ? page.name_ar : page.name_en,
+		content: req.params.lang == 'ar' ? page.html_ar : page.html_en,
+	};
+
+	res.render('static_page', {
 		project_name:	settings.project_name,
-		title:			'My wallet Chart',
-		content:		'',
-	});*/
+		title:			html.title,
+		content:		html.content,
+	});
 });
 
 module.exports = app;
