bb59236f73e9aca1df5e61bcfc5aa7bad7cee1a9
s.m.hejazi <hejazi.ppp@gmail.com>
2020-05-31 17:29:39 +0430

    add data in notification show in App

--- core/models/Transaction.js
+++ core/models/Transaction.js
@@ -588,80 +588,78 @@ TransactionSchema.static({
             {
                 $group: {
                     _id: null,
-
                     items: {$push: '$$ROOT'},
                     total: {$sum: 1}
                 }
             },
             // get Total
-            {
-                $lookup: {
-                    from: 'transactions',
-                    pipeline: [
-                        {$match: criteria},
-                        {$match: optFilter.filters},
-                        {
-                            $lookup: {
-                                from: 'users',
-                                let: {primaryUserId: "$userId"},
-                                pipeline: [
-                                    {$match: {$expr: {$eq: ["$$primaryUserId", "$_id"]}}},
-                                ],
-                                as: 'getUser'
-                            }
-                        },
-                        {$unwind: {path: "$getUser", preserveNullAndEmptyArrays: false}},
-                        {
-                            $lookup: {
-                                from: 'bankaccounts',
-                                let: {primaryAccountId: "$accountId"},
-                                pipeline: [
-                                    {$match: {$expr: {$eq: ["$$primaryAccountId", "$_id"]}}},
-                                    {
-                                        $lookup: {
-                                            from: 'banknames',
-                                            foreignField: '_id',
-                                            localField: 'bankNameId',
-                                            as: "getBankName"
-                                        }
-                                    },
-                                    //get bank name
-                                    {
-                                        $project: {
-                                            _id: 0,
-                                            id: '$_id',
-                                            fullName: {$concat: ['$firstname', ' ', '$lastname']},
-                                            IBAN: 1,
-                                            civilId: 1,
-                                            bankName: {$arrayElemAt: ['$getBankName.name_en', 0]}
-                                        }
-                                    },
-                                ],
-                                as: 'getAccount'
-                            }
-                        },
-                        {$match: strMatch},
-                        {$match: NumMatch},
-                        {$count: 'total'},
-                    ],
-                    as: 'getTotal'
-                }
-            },
+            // {
+            //     $lookup: {
+            //         from: 'transactions',
+            //         pipeline: [
+            //             {$match: criteria},
+            //             {$match: optFilter.filters},
+            //             {
+            //                 $lookup: {
+            //                     from: 'users',
+            //                     let: {primaryUserId: "$userId"},
+            //                     pipeline: [
+            //                         {$match: {$expr: {$eq: ["$$primaryUserId", "$_id"]}}},
+            //                     ],
+            //                     as: 'getUser'
+            //                 }
+            //             },
+            //             {$unwind: {path: "$getUser", preserveNullAndEmptyArrays: false}},
+            //             {
+            //                 $lookup: {
+            //                     from: 'bankaccounts',
+            //                     let: {primaryAccountId: "$accountId"},
+            //                     pipeline: [
+            //                         {$match: {$expr: {$eq: ["$$primaryAccountId", "$_id"]}}},
+            //                         {
+            //                             $lookup: {
+            //                                 from: 'banknames',
+            //                                 foreignField: '_id',
+            //                                 localField: 'bankNameId',
+            //                                 as: "getBankName"
+            //                             }
+            //                         },
+            //                         //get bank name
+            //                         {
+            //                             $project: {
+            //                                 _id: 0,
+            //                                 id: '$_id',
+            //                                 fullName: {$concat: ['$firstname', ' ', '$lastname']},
+            //                                 IBAN: 1,
+            //                                 civilId: 1,
+            //                                 bankName: {$arrayElemAt: ['$getBankName.name_en', 0]}
+            //                             }
+            //                         },
+            //                     ],
+            //                     as: 'getAccount'
+            //                 }
+            //             },
+            //             {$match: strMatch},
+            //             {$match: NumMatch},
+            //             {$count: 'total'},
+            //         ],
+            //         as: 'getTotal'
+            //     }
+            // },
             {
                 $project: {
                     _id: 0,
                     items: 1,
-                    total: {$arrayElemAt: ["$getTotal", 0]},
+                    total: 1//{$arrayElemAt: ["$getTotal", 0]},
                 }
             },
         ])
             .then(async result => {
-                console.info("&&&&&&&&&&&&&&&&&&&&&&&&&&&& transactrion result: ", result);
                 console.info("&&&&&&&&&&&&&&&&&&&&&&&&&&&& transactrion result %j: ", result);
                 let items = [],
                     total = 0;
                 if (result.length > 0) {
-                    total = result[0].total? result[0].total.total : 0;
+                    total = result[0].total ? result[0].total : 0;
                     delete result[0].total;
                     items = result[0].items;
                 }
